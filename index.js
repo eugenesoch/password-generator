d7431024dbe1634e8cbb6a4e54b2e4d101b4de6f
Jevgenij Soch <110429125+eugenesoch@users.noreply.github.com>
2024-01-18 18:33:32 +0000

    Major update
    Created styles in CSS, created layout and variables for the dark mode.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+//Requirements:
+//Build it from scratch
+//Generate two random passwords when the user clicks the button
+//Each password should be 15 characters long
+
+
+//Stretch goals
+//Ability to set password lentgh
+//Add "copy-on-click"
+//Toggle "symbols" and "numbers" on/off
+//Choose between light or dark mode, or create a switch :))
+
+//help section url: https://projects.scrimba.com/password-generator
+
+
+
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 

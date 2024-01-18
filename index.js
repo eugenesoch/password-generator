b63e551485403862bd151136e4fab0d8cf5407f2
Jevgenij Soch <110429125+eugenesoch@users.noreply.github.com>
2024-01-18 18:55:27 +0000

    Changes to js file
    changes to JS file

--- index.js
+++ index.js
@@ -12,11 +12,16 @@
 
 //help section url: https://projects.scrimba.com/password-generator
 
+//dark mode source: https://lukelowrey.com/css-variable-theme-switcher/
+
 
 
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+function generatePassword () {
+    console.log("Generating the best password")
+}
 
 
 

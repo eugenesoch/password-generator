78383c5ea4390a883ba462a6b7029453a2666129
Jevgenij Soch <110429125+eugenesoch@users.noreply.github.com>
2024-01-19 17:05:02 +0000

    Added them toggle button
    Styling is fixed.

--- index.js
+++ index.js
@@ -49,20 +49,6 @@ function generateRandomPasswords() {
 
 }
 
-function copyText() {
-    // Get the text field
-    var copyText = document.getElementById("password-el1");
-  
-    // Select the text field
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
-  
-     // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.value);
-  
-    // Alert the copied text
-    alert("Copied the text: " + copyText.value);
-  }
 
 var toggle = document.getElementById("theme-toggle");
 
